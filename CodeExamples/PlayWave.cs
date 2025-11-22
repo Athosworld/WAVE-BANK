eb5dedc1746759d016aff1ef5ddaf508a1b22e45
Athos García <yuniaobird@gmail.com>
2025-11-21 16:16:07 -0800

    C# code example

---
+++ CodeExamples/PlayWave.cs
@@ -0,0 +1,21 @@
+//This example just plays a wave stored in a wave bank, fetching it with its ID
+using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            var asm = Assembly.LoadFrom(AppDomain.CurrentDomain.BaseDirectory + "WAVEBANK.dll");
+            var type = asm.GetType("WAVEBANK.WBKPlayerLib.Player");
+            var player = Activator.CreateInstance(type);
+            type.GetMethod("LoadWBK").Invoke(player, new object[] { ".wbk" }); //Replace this with the path to the bank
+            type.GetMethod("PlayWave").Invoke(player, new object[] { "001" });
+        }
+        catch { }
+
+        Console.ReadKey();
+    }
+}
